da9f1b6ff5f63877f41dcf3e6071f97e52c82303
chance-stribling <cstribling1717@gmail.com>
2023-02-27 10:57:16 -0500

    npm vue create, removed templates, added navbar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import TopBar from './components/TopBar.vue';
+</script>
+
+<template>
+  <TopBar/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+    data() {
+        return{
+            title: "Snackr Trackr"
+        }
+    }
+})
+</script>
+
+<template>
+    <v-layout>
+
+        <v-toolbar
+        dark
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-title>hello</v-app-bar-title>
+        </v-toolbar>
+    </v-layout>
+</template>
+
+<style scoped>
+</style>
